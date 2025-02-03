69db285873b4591cacc75631708d2092f26b9792
vincentmartin84 <vinmart@outlook.fr>
2025-02-03 11:40:05 +0100

    creation game function

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ from functions import compatible_letters
 from functions import word_input
 from functions import remove_accents
 
-random_word= remove_accents(generate_word())
-print(random_word)
+def game():
+    random_word = remove_accents(generate_word())
+    print(random_word)
+
+    x = 1
+    while x < 4:
+        print(f"essai {x}")
+        user_word = word_input()
+
+        if random_word == user_word:
+            print(f"Bravo vous avez trouvé le mot {random_word}")
+            break
+
+        compatible_letters(random_word, user_word)
+        print()
+        x = x + 1
+
+    if random_word != user_word:
+        print(f"Désolé, vous n'avez pas trouvé le mot. Le mot était : {random_word}")
 
 
-
-x = 0
-while x<8:
-    user_word = word_input()
-
-    if random_word == user_word:
-        print(f"Bravo vous avez trouvé le mot {random_word}")
-        break
-
-    compatible_letters(random_word,user_word)
-    print()
-
-
+game()
